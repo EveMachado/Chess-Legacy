23a051d55fb0b7f71dbcc2d88b30253c0ef745ec
Evellyn Machado <fernandaevellyn337@gmail.com>
2025-02-18 22:27:24 -0300

    Movendo as peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,17 +3,33 @@ package application;
 import boardgame.Board;
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
 
         //função para imprimir as peças da partida
 
         //classe User Interface e dentro dela o método printBoard
-        UI.printBoard(chessMatch.getPieces());
+        while(true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Origem: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Alvo: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 
 }
